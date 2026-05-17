e7c40571018420cd9a87f065f60cd325730c7d13
Mdibr135 <114769267+Mdibr135@users.noreply.github.com>
2026-05-17 18:32:34 +0500

    Change sleep duration from 5 to 30 seconds
    Increase sleep duration before starting the Telegram bot.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 # Запускаем FastAPI на порту, который выделит Render ($PORT)
 echo "Запуск FastAPI бэкенда..."
 uvicorn app.main_server:app --host 0.0.0.0 --port $PORT &
-sleep 5
+sleep 30
 echo "Запуск Телеграм-бота..."
 python bot/main_bot.py
