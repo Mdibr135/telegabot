f06b942df3648f14c2643a78114d1a1eced76864
Mdibr135 <114769267+Mdibr135@users.noreply.github.com>
2026-05-17 20:46:29 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 # Запускаем FastAPI на порту, который выделит Render ($PORT)
 echo "Запуск FastAPI бэкенда..."
 uvicorn app.main_server:app --host 0.0.0.0 --port $PORT &
-sleep 30
+sleep 90
 echo "Запуск Телеграм-бота..."
 python bot/main_bot.py
