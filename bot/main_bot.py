965d1579f7bcdc0a6aaf49fd725170644931a751
Mdibr135 <114769267+Mdibr135@users.noreply.github.com>
2026-05-17 15:50:52 +0500

    Change backend URL to production server
    Updated backend URL for user registration.

--- bot/main_bot.py
+++ bot/main_bot.py
@@ -14,7 +14,7 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 # Адрес нашего запущенного бэкенда (FastAPI)
 # Пока тестируем локально, адрес такой. Когда выложим в интернет, поменяем на домен.
-BACKEND_URL = "http://127.0.0.1:8000/users/"
+BACKEND_URL = "https://telegabot-77m2.onrender.com/users/"
 
 # Настраиваем логирование, чтобы видеть в терминале, если что-то пойдет не так
 logging.basicConfig(level=logging.INFO)
